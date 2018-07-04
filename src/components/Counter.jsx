0177a6286786f9a352c38888132167f220387785
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2018-07-04 12:20:48 +0300

    Move react-hot-loader to devDependencies. Small change to example.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 
 const Counter = ({ count, increment, decrement, incrementAsync }) => (
   <Fragment>
-    <h1>{count}s</h1>
+    <h1>{count}</h1>
     <button onClick={increment}>Increment</button>
     <button onClick={decrement}>Decrement</button>
     <button onClick={incrementAsync}>Increment Async</button>
